7fcb95a3da80b305cc4b22880ec81358bfbb85d2
Pavlo Andriiesh <pavlo.andriiesh@gmail.com>
2019-02-01 16:09:13 +0200

    log render seen indexes error

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,12 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    try {
+        return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    } catch (err) {
+    	console.log('Error rendering seen indexes', err, this.state.seenIndexes, this.state);
+    	return [];
+    }
   }
 
   renderValues() {
