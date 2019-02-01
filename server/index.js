bdd9c9e5c838ae04bd8c2f1fdb9def3d0f0c2c6e
Pavlo Andriiesh <pavlo.andriiesh@gmail.com>
2019-02-01 16:46:36 +0200

    test to fix bug with ingress kubernetes rewrite

--- server/index.js
+++ server/index.js
@@ -35,17 +35,17 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hi');
 });
 
-app.get('/api/values/all', async (req, res) => {
+app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
 
-app.get('/api/values/current', async (req, res) => {
+app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
